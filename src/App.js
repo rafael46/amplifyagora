b1ecff3852ef688cf76adbcdd91fb712bea14faf
rafael cervantes <rafael@vipmarketingla.com>
2019-01-20 22:43:13 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,108 @@
 import React from "react";
+import { Auth, Hub } from "aws-amplify";
+import { Authenticator, AmplifyTheme } from "aws-amplify-react";   //withAuthenticator
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import  HomePage  from "./pages/HomePage";
+import ProfilePage from "./pages/ProfilePage";
+import  MarketPage  from "./pages/MarketPage";
+import Navbar from "./components/Navbar";
+
 import "./App.css";
+// import { SectionHeader } from "aws-amplify-react/dist/AmplifyUI";
+import { isThisYear } from "date-fns";
+
 
 class App extends React.Component {
-  state = {};
+  state = {
+    user: null,
+  };
+
+  componentDidMount(){
+    console.dir(AmplifyTheme);
+    this.getUserData();
+    Hub.listen('auth', this, 'onHUbCapsule');
+
+  }
+
+  getUserData = async () => {
+    const user = await Auth.currentAuthenticatedUser()
+    user ? this.setState({user}) : this.setState({user : null })
+
+
+  }
+
+  onHubCapsule = capsule => {
+    switch (capsule.payload.event) {
+      case "signIn":
+        console.log('signed in')
+        isThisYear.getUserData()
+        
+        break;
+      case "signUp":
+        console.log('signed up');
+        break;
+      case "signOut":
+        console.log('signed out');
+        this.setState({user: null});
+        break;
+
+      default:
+        return;
+        // break;
+    }
+    // capsule.payload.event
+  }
 
   render() {
-    return <div>App</div>;
-  }
-}
+    const {user} = this.state;
 
+    //<div>App</div>;
+    return !user ? (
+      <Authenticator theme={theme}/>
+    ) : (
+      <Router>
+        <>
+          {/* <HomePage/> */}
+          {/* navbar */}
+          <Navbar />
+          {/* Routes */}
+          <div className= "app-container">
+            <Route exact path= "/" Component = { HomePage } />
+            <Route path = "/profile" Component = {ProfilePage}/>
+            {/* .params.marketId */}
+            <Route path = "/markets/:marketId" Component={({ match }) => <MarketPage marketId = {match.params.marketId} /> }/>    
+
+
+          </div>
+
+        </>
+
+      </Router>
+      );
+  }
+} // end class 
+
+const theme = {
+  ...AmplifyTheme,
+  navBar: {
+    ...AmplifyTheme.navBar,
+    backgroundColor: "#ffc0cb"
+  },
+  button: {
+    backgroundColor: "var(--amazonOrange)"
+  },
+  sectionBody: {
+    ...AmplifyTheme.sectionBody,
+    padding: "5px"
+
+  },
+  sectionHeader:{
+    ...AmplifyTheme.sectionHeader,
+    backgroundColor: "var(--squidInk)"
+  }
+};
+
+// export default withAuthenticator( App, true, [], null, theme);   // { includeGreetings: true}
 export default App;
+
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,26 @@
 import React from "react";
-// import { Menu as Nav, Icon, Button } from "element-react";
+import { Menu as Nav, Icon, Button } from "element-react";
+import { NavLink } from "react-router-dom";
 
-const Navbar = () => <div>Navbar</div>;
+const Navbar = () => (
+  <Nav mode="horizontal" theme= "dark" defaultActive = "1">
+    <div className="nav-container">
+      {/* App title icon */}
+      <Nav.Item index ="1">
+        <NavLink to="/" className = "nav-link">
+          <span className="app-title">
+            <img src="https://icon.now.sh/account_balance/f90" alt="App Icon" className="app-icon"/>
+              AmplifyAgora
+          </span>
+
+        </NavLink>
+      </Nav.Item>
+      {/* video minute 9 here */}
+
+    </div>
+
+  </Nav>
+
+);
 
 export default Navbar;
